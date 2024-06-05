3a2f356574f37406bc1e4ecd352ede586b80926d
TimotheeSendra <lola.schuler.iut@gmail.com>
2024-06-05 15:45:13 +0200

    Finalisation des scripts - terminés et fonctionnels

--- e105_data_extraction.sh
+++ e105_data_extraction.sh
@@ -2,7 +2,7 @@
 #gets data from MQTT broker
 
 #gets room E105 sensor's name (AM107-35)
-capteur=$(cat ~/Documents/GitHub/SAE23/capteurs | cut -d " " -f 4)
+capteur=$(cat /home/etud/Documents/GitHub/SAE23/capteurs | cut -d " " -f 4)
 
 #gets exactly one message from the sensor thanks to the MQTT broker
 message=$(mosquitto_sub -h mqtt.iut-blagnac.fr -t AM107/by-deviceName/$capteur/# -C 1)
@@ -13,4 +13,4 @@ date=$(date +%Y-%m-%d)
 heure=$(date +%T)
 
 #launches the data sending script
-bash ./data_insertion $valeur $date $heure $capteur
+bash /home/etud/Documents/GitHub/SAE23/data_insertion $valeur $date $heure $capteur
